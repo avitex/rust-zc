3dc72b69cb46d28d427204825d5204a05822bd52
avitex <theavitex@gmail.com>
2021-01-22 10:44:11 +1100

    add test for fetching dependant

---
+++ tests/invalid-use/get_dependant_with_static.rs
@@ -0,0 +1,20 @@
+use zc::Dependant;
+
+#[derive(Dependant, PartialEq, Debug)]
+pub struct MyStruct<'a>(&'a [u8]);
+
+impl<'a> From<&'a [u8]> for MyStruct<'a> {
+    fn from(bytes: &'a [u8]) -> Self {
+        Self(&bytes)
+    }
+}
+
+fn main() {
+    let owner = vec![1, 2, 3];
+    let data = zc::from!(owner, MyStruct, [u8]);
+    let dependant_ref = data.get::<MyStruct<'static>>();
+    assert_eq!(
+        dependant_ref,
+        &MyStruct(&[1, 2, 3])
+    )
+}
