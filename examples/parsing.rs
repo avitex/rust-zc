3ecce7f79c0e7311e6b03f69c826c308d584e2dd
avitex <theavitex@gmail.com>
2021-02-18 15:31:37 +1100

    fix parsing example

--- examples/parsing.rs
+++ examples/parsing.rs
@@ -1,16 +1,16 @@
 use dangerous::{BytesReader, Fatal, Input};
 use zc::Zc;
 
-fn parse_from_bytes(bytes: &[u8]) -> Result<Vec<&str>, ()> {
+fn main() {
+    let buf = Vec::from(&b"thisisatag,thisisanothertag"[..]);
+    let parsed = Zc::new(buf, parse_bytes);
+    dbg!(parsed.into_result().unwrap());
+}
+
+fn parse_bytes(bytes: &[u8]) -> Result<Vec<&str>, ()> {
     dangerous::input(bytes)
         .read_all(parse)
         .map_err(|_: Fatal| ())
-}
-
-fn main() {
-    let buf = Vec::from(&b"thisisatag,thisisanothertag"[..]);
-    let parsed = Zc::new(buf, parse_from_bytes);
-    dbg!(parsed.into_result().unwrap());
 }
 
 fn parse<'i, E>(r: &mut BytesReader<'i, E>) -> Result<Vec<&'i str>, E>
@@ -19,7 +19,7 @@ where
 {
     let mut parts = Vec::new();
     loop {
-        let s = r.take_while(|b| b != b',').to_dangerous_str::<E>()?;
+        let s = r.take_until_opt(b',').to_dangerous_str::<E>()?;
         parts.push(s);
         if !r.consume_opt(b',') {
             return Ok(parts);
